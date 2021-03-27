28b077e7777a69e602d652ef043b40d087cb0c74
Nurahmadie <177826+fudanchii@users.noreply.github.com>
2021-03-27 17:58:20 +0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+pub mod ledger;
 pub mod parser;
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,14 @@
+use crate::ledger::Ledger;
+use pest::error::Error;
+
 #[derive(Parser)]
 #[grammar = "ledger.pest"]
-pub struct LedgerParser;
+pub struct LedgerParser {
+    ledger: Ledger,
+}
+
+impl LedgerParser {
+    pub fn try_into(self) -> Result<Ledger, Error<Rule>> {
+        Ok(Ledger)
+    }
+}
